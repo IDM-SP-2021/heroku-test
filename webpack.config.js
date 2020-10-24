926388396017d6115f5269be8acc6ea19e104801
Erik-Martus <ekm66@drexel.edu>
2020-10-24 14:51:36 -0400

    :wrench: Add webpack config setup

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const buildDir = path.join(__dirname, 'build');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: 'app.js',
+    path: buildDir,
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: buildDir,
+    port: 3000,
+    open: 'Firefox Developer Edition'
+  },
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({template: 'src/index.html'})
+  ],
+
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
