b2fa6f8a438affe8c9ed712a6752fb2d29819b47
Erik-Martus <ekm66@drexel.edu>
2020-10-26 15:41:49 -0400

    :construction: Change cypher query to list relationships

--- src/app.js
+++ src/app.js
@@ -16,55 +16,89 @@ $(function () {
 const renderGraph = () => {
   var width = 800, height = 800;
 
-  var data = api.getGraph();
-  console.log(data);
+  var svg = d3.select('#graph').append('svg')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    .attr('pointer-events', 'all');
 
-  // const links = data.links.map(d => Object.create(d));
-  // const nodes = data.p.map(d => Object.create(d));
+  api
+    .getGraph()
+    .then(graph => {
+      const links = graph.links.map(d => Object.create(d));
+      const nodes = graph.nodes.map(d => Object.create(d));
 
-  // const simulation = d3.forceSimulation(nodes)
-  //     .force("link", d3.forceLink(links).id(d => d.id))
-  //     .force("charge", d3.forceCenter(width /2, height / 2));
+      const simulation = d3.forceSimulation(nodes)
+          .force('link', d3.forceLink(links).distance(60))
+          .force('charge', d3.forceCenter(width / 2, height / 2));
 
-  // const svg = d3.create('svg')
-  //     .attr("viewbox", [0, 0, width, height]);
+      // const svg = d3.create('svg')
+      //     .attr('viewbox', [0, 0, width, height]);
 
-  // const link = svg.append('g')
-  //     .attr('stroke', '#999')
-  //     .attr('stroke-opacity', 0.6)
-  //   .selectAll('line')
-  //   .data(links)
-  //   .join('line')
-  //     .attr('stroke-width', d => Math.sqrt(d.value));
+      const link = svg.append('g')
+          .attr('stroke', '#999')
+          .attr('stroke-opacity', 0.6)
+          .attr('class', 'links')
+        .selectAll('line')
+        .data(links)
+        .join('line')
+          .attr('stroke-width', '3');
 
-  // const node = svg.append('g')
-  //     .attr('stroke', '#fff')
-  //     .attr('stroke-width', 1.5)
-  //   .selectAll('circle')
-  //   .data(nodes)
-  //   .join('circle')
-  //     .attr('r', 5)
-  //     .attr('fill', color)
-  //     .call(drag(simulation));
+      const node = svg.append('g')
+          .attr('stroke', '#fff')
+          .attr('stroke-width', 1.5)
+          .attr('class', 'node')
+        .selectAll('circle')
+        .data(nodes)
+        .join('circle')
+          .attr('r', 10)
+          .attr('fill', '#3BCEAC')
+          .call(drag(simulation));
 
-  // node.append('title')
-  //     .text(d => d.name);
+      node.append('title')
+        .text(d => {
+          return d.title;
+        });
 
-  // simulation.on('tick', () => {
-  //   link
-  //       .attr('x1', d => d.source.x)
-  //       .attr('y1', d => d.source.y)
-  //       .attr('x2', d => d.target.x)
-  //       .attr('y2', d => d.target.y);
+        simulation.on('tick', () => {
+          link
+            .attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y)
 
-  //   node
-  //       .attr('cx', d => d.x)
-  //       .attr('cy', d => d.y);
-  // });
+          node
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y)
+        });
 
-  // invalidation.then(() => simulation.stop());
+        // invalidation.then(() => simulation.stop());
 
-  // return svg.node();
+        return svg.node();
+    })
+
+    const drag = simulation => {
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      }
+
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
+
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
+
+      return d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+    }
 }
 
 // const renderGraph = () => {
--- src/scripts/neo4j.js
+++ src/scripts/neo4j.js
@@ -4,31 +4,20 @@ var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "ab
 function getGraph() {
   var session = driver.session();
   return session.run(
-    'MATCH (p:Person)-[r]->(a:Person) \
-    RETURN p.name AS name, collect(a.name) AS rel_name, type(r) AS rel_type'
+    'MATCH (s:Person)-[r]->(t:Person) \
+    RETURN ID(s) AS src_id, ID(t) AS tar_id, type(r) AS rel_type'
   )
   .then(results => {
     var nodes = [], rels = [], i = 0;
     results.records.forEach(res => {
-      // console.log(res.get('name'));
-      nodes.push({title: res.get('name'), label: 'Person'});
-      var target = i;
-      i++;
+      // console.log(`${res.get('name')} is ${res.get('rel_type')} to ${res.get('rel_name')}`)
 
-      res.get('rel_name').forEach(name => {
-        var rel = {title: name, label: 'Person'};
-        var source = _.findIndex(nodes, rel);
-        if (source === -1) {
-          nodes.push(rel);
-          source = i;
-          i++;
-        }
-        rels.push({source, target});
-      })
+      var source = res.get('src_id').low;
+      var target = res.get('tar_id').low;
+
+      rels.push({source, target});
+      console.log(rels)
     });
-
-    console.log('nodes: ', nodes);
-    console.log('relationships: ', rels);
 
     return {nodes, links: rels};
   })
